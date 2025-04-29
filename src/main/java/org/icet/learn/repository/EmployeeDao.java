b90a36d4232f5fcfbb5d3349124b78e3f354ddb6
Sithum9999 <sankalpasithum51@gmail.com>
2025-04-29 11:27:58 +0530

    create existsByEmail method in repo layer

--- src/main/java/org/icet/learn/repository/EmployeeDao.java
+++ src/main/java/org/icet/learn/repository/EmployeeDao.java
@@ -4,4 +4,5 @@ import org.icet.learn.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeDao extends JpaRepository<EmployeeEntity,Long> {
+    boolean existsByEmail(String email);
 }
