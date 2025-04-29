7697a74083430d872c98b4b85f7fbdde79017db6
Sithum9999 <sankalpasithum51@gmail.com>
2025-04-29 10:49:34 +0530

    created the EmployeeServiceImpl class

---
+++ src/main/java/org/icet/learn/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,50 @@
+package org.icet.learn.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.icet.learn.dto.Employee;
+import org.icet.learn.entity.EmployeeEntity;
+import org.icet.learn.repository.EmployeeDao;
+import org.icet.learn.service.EmployeeService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeDao employeeDao;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public List<Employee> all() {
+        List<Employee> employeeList = new ArrayList<>();
+        List<EmployeeEntity> all = employeeDao.findAll();
+        all.forEach(customerEntity -> {
+            employeeList.add(modelMapper.map(customerEntity,Employee.class));
+        });
+        return employeeList;
+    }
+
+    @Override
+    public void add(Employee employee) {
+        employeeDao.save(modelMapper.map(employee, EmployeeEntity.class));
+    }
+
+    @Override
+    public Employee search(Long id) {
+        return modelMapper.map(employeeDao.findById(id),Employee.class);
+    }
+
+    @Override
+    public void update(Employee employee) {
+        employeeDao.save(modelMapper.map(employee, EmployeeEntity.class));
+    }
+
+    @Override
+    public void delete(Long id) {
+        employeeDao.deleteById(id);
+    }
+}
