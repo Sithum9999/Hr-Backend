e575b07908b51f33cb57028cc74afa0f481f0fa8
Sithum9999 <sankalpasithum51@gmail.com>
2025-04-29 12:55:55 +0530

    add Some validation annotation

--- src/main/java/org/icet/learn/dto/Employee.java
+++ src/main/java/org/icet/learn/dto/Employee.java
@@ -1,6 +1,8 @@
 package org.icet.learn.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
+    @NotBlank
     private Long id;
+    @NotBlank
+    @Size(max = 100)
     private String name;
+    @NotBlank
     @Column(unique = true)
     private String email;
+    @NotBlank
     private String department;
+    @NotBlank
     private String createdAt;
+    @NotBlank
     private String updatedAt;
 }
--- src/main/java/org/icet/learn/entity/EmployeeEntity.java
+++ src/main/java/org/icet/learn/entity/EmployeeEntity.java
@@ -1,6 +1,7 @@
 package org.icet.learn.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +15,15 @@ public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     @Column(unique = true)
     private String email;
+    @NotBlank
     private String department;
+    @NotBlank
     private String createdAt;
+    @NotBlank
     private String updatedAt;
 }
